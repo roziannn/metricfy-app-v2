67148c993cebc37800974f31ed23bb6f3f3bb2af
Firda Rosiana T <firdaroziana10@gmail.com>
2025-10-01 18:04:41 +0700

    feat(auth/db): integrate better-auth (GitHub OAuth) and setup db  with Neon ,prisma, T3 env

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,15 @@
+import { headers } from "next/headers";
+import { LoginForm } from "./_components/LoginForm";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function LoginPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (session) {
+    return redirect("/");
+  }
+  return <LoginForm />;
+}
