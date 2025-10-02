8739180e048340261287500e7ef29da767cd7109
Firda Rosiana T <firdaroziana10@gmail.com>
2025-10-03 06:27:20 +0700

    feat(landing): major platform layout update and refactored components

---
+++ app/(platform)/_components/Navbar.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { buttonVariants } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
+import { authClient } from "@/lib/auth-client";
+import { UserDropdown } from "./UserDropdown";
+import Link from "next/link";
+
+const navigationItems = [
+  { name: "Home", href: "/" },
+  { name: "Courses", href: "/courses" },
+  { name: "Dashboard", href: "/dashboard" },
+];
+
+export function Navbar() {
+  const { data: session, isPending } = authClient.useSession();
+
+  return (
+    <header
+      className="sticky top-0 z-50 w-full border-b bg-background/95 
+    backdrop-blur-[backdrop-filter]:bg-background/60"
+    >
+      <div className="container flex min-h-16 items-center mx-auto px-4 md:px-6 lg:px-8">
+        <Link href="/" className="flex items-center space-x-2 mr-4">
+          <span className="font-bold">Metricfy.</span>
+        </Link>
+
+        {/* desktop navigation */}
+        <nav className="hidden md:flex md:flex-1 md:items-center md:justify-between">
+          <div className="flex items-center space-x-2">
+            {navigationItems.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                className="text-sm font-medium
+                    transition-colors hover:text-primary"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
+          <div className="flex items-center space-x-4">
+            <ThemeToggle />
+            {isPending ? null : session ? (
+              <UserDropdown
+                email={session.user.email}
+                name={session.user.name}
+                image={session.user.image || ""}
+              />
+            ) : (
+              <>
+                <Link
+                  href="/login"
+                  className={buttonVariants({ variant: "secondary" })}
+                >
+                  Login
+                </Link>
+                <Link href="/login" className={buttonVariants()}>
+                  Get Started
+                </Link>
+              </>
+            )}
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
---
+++ app/(platform)/_components/UserDropdown.tsx
@@ -0,0 +1,114 @@
+import {
+  BookOpen,
+  ChevronDownIcon,
+  Home,
+  LayoutDashboard,
+  LogOutIcon,
+  Settings2,
+} from "lucide-react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+interface iAppProps {
+  name: string;
+  email: string;
+  image: string;
+}
+
+export function UserDropdown({ email, name, image }: iAppProps) {
+  const router = useRouter();
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+          toast.success("Singed out successfully!");
+        },
+        onError: () => {
+          toast.success("Failed to sing out");
+        },
+      },
+    });
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-auto p-0 hover:bg-transparent">
+          <Avatar>
+            <AvatarImage src={image} alt="Profile image" />
+            <AvatarFallback>{name[0].toUpperCase()}</AvatarFallback>
+          </Avatar>
+          <ChevronDownIcon
+            size={16}
+            className="opacity-60"
+            aria-hidden="true"
+          />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="max-w-64">
+        <DropdownMenuLabel className="flex min-w-0 flex-col">
+          <span className="text-foreground truncate text-sm font-medium">
+            {name}
+          </span>
+          <span className="text-muted-foreground truncate text-xs font-normal">
+            {email}
+          </span>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            <Link href="/">
+              <Home size={16} className="opacity-60" aria-hidden="true" />
+              <span>Home</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="/course">
+              <BookOpen size={16} className="opacity-60" aria-hidden="true" />
+              <span>Course</span>
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="/dashboard">
+              <LayoutDashboard
+                size={16}
+                className="opacity-60"
+                aria-hidden="true"
+              />
+              <span>Dashboard</span>
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem asChild>
+            <Link href="/settings">
+              <Settings2 size={16} className="opacity-60" aria-hidden="true" />
+              <span>Settings</span>
+            </Link>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={signOut}>
+          <LogOutIcon size={16} className="opacity-60" aria-hidden="true" />
+          <span>Logout</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
